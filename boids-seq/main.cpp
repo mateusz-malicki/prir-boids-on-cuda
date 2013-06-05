f90a8954b2d0020307f15b0d1bc89fd0395b9bd8
Mateusz Ochtera <spaczn@gmail.com>
2013-06-05 17:04:17 +0200

    Pobieraj parametry wejściowe

--- boids-seq/main.cpp
+++ boids-seq/main.cpp
@@ -1,5 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+int WINDOW_SIZE;
+int NUM_BOIDS;
+int MAX_TIME;
+int NUM_FRAMES;
 
 int main(int argc, char** argv){
+
+//Pobierz ustawienia poczatkowe z wejścia
+  if(argc < 5){
+    puts("Za mało argumentów");
+    puts("boids WINDOW_SIZE NUM_BOIDS MAX_TIME NUM_FRAMES");
+    exit(1);
+  }else{
+    WINDOW_SIZE = atoi(argv[1]);
+    NUM_BOIDS = atoi(argv[2]);
+    MAX_TIME = atoi(argv[3]);
+    NUM_FRAMES = atoi(argv[4]);
+
+    if( WINDOW_SIZE == 0 || NUM_BOIDS == 0 || MAX_TIME == 0 || NUM_FRAMES == 0 ){
+      puts("Parametry muszą być liczbami całkowitymi");
+      exit(1);
+    }
+  }
+
+  printf("Okno ma rozmiar: %dx%d, posiada %d boidów, pracuje max %ds lub aż wygeneruje %d klatek\n",WINDOW_SIZE, WINDOW_SIZE, NUM_BOIDS, MAX_TIME, NUM_FRAMES);
   return 1;
 }
