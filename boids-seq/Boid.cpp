8cb822548ae781fae01f344c95a2b77d3c05b9f0
Mateusz Ochtera <spaczn@gmail.com>
2013-06-05 19:20:28 +0200

    Dodaj klasę Boid

---
+++ boids-seq/Boid.cpp
@@ -0,0 +1,6 @@
+#include "Boid.h"
+
+Boid::Boid(int x, int y){
+  position = Point2D(x,y);
+}
+
---
+++ boids-seq/Boid.h
@@ -0,0 +1,13 @@
+#ifndef BOID_H
+#define BOID_H
+
+#include "ser_struct.h"
+
+#define RANGE 32;
+
+class Boid{
+  Point2D position;
+  Boid(int, int);
+};
+
+#endif
