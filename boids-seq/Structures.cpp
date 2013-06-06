3dafa6c41346912785aa742e15f14dfa10f187a3
Mateusz Malicki <mateusz.malicki1991@gmail.com>
2013-06-06 03:03:33 +0200

    Napraw błędy z template functions

--- boids-seq/Structures.cpp
+++ boids-seq/Structures.cpp
@@ -1,24 +1,30 @@
 #include "Structures.h"
-  template<class Archive>
-  void Point2D::serialize(Archive & ar, const unsigned int version)
-  {
-    ar & BOOST_SERIALIZATION_NVP(x)
-       & BOOST_SERIALIZATION_NVP(y);
-  }
-
+ 
   Point2D::Point2D(int a, int b)
   {
     x = a;
     y = b;
   }
 
-  template<class Archive>
-  void Frame::serialize(Archive & ar, const unsigned int version)
-  {
-    ar & BOOST_SERIALIZATION_NVP(pos);
+  float Vector2D::getAngle()
+  { //v2 = {1,0}
+    //angle(v1, v2) = acos( (v1x * v2x + v1y * v2y) / (sqrt(v1x^2+v1y^2) * sqrt(v2x^2+v2y^2)) )
+    return acos( x/(sqrt(x*x+y*y)) );
   }
 
-  void Frame::add_point(Point2D point)
+  void Frame::addPoint(Point2D point)
   {
-      pos.push_back(point);
+      positions.push_back(point);
+      separations.push_back(Vector2D(0,0));
+      alignments.push_back(Vector2D(0,0));
+      cohesions.push_back(Vector2D(0,0));
   }
+
+  void Frame::addPoint()
+  {
+      positions.push_back(Point2D(0,0));
+      separations.push_back(Vector2D(0,0));
+      alignments.push_back(Vector2D(0,0));
+      cohesions.push_back(Vector2D(0,0));
+  }
+
--- boids-seq/Structures.h
+++ boids-seq/Structures.h
@@ -10,15 +10,41 @@
 #include <boost/serialization/list.hpp>
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/archive/xml_iarchive.hpp>
+#include <cmath>
 
 using namespace std;
+
+class Vector2D
+{
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version)
+  {
+    ar & BOOST_SERIALIZATION_NVP(x)
+       & BOOST_SERIALIZATION_NVP(y);
+  }
+
+public:
+  float x;
+  float y;
+  inline float getLength() { return sqrt(x*x+y*y); };
+  float getAngle(); 
+  Vector2D(){};
+  Vector2D(float , float);
+  Vector2D operator+ (Vector2D &addend) {return Vector2D(x+addend.x, y+addend.y); };  //yup, lubimy przeciążać te oczywiste operatory
+};
 
 class Point2D
 {
 private:
   friend class boost::serialization::access;
-  template<class Archive>
-  void serialize(Archive & ar, const unsigned int version);
+  template<class Archive> 
+  void serialize(Archive & ar, const unsigned int version)
+  {
+    ar & BOOST_SERIALIZATION_NVP(x)
+       & BOOST_SERIALIZATION_NVP(y);
+  }
 
 public:
   int x;
@@ -32,12 +58,18 @@ class Frame
 private:
   friend class boost::serialization::access;  
   template<class Archive>
-  void serialize(Archive & ar, const unsigned int version);
-
+  void serialize(Archive & ar, const unsigned int version)
+  {
+    ar & BOOST_SERIALIZATION_NVP(pos);
+  }
 
 public:
-  list<Point2D>pos;
+  list<Point2D>positions;
+  list<Vector2D>separetions;
+  list<Vector2D>alignments;
+  list<Vector2D>cohesions;
   Frame(){};
-  void add_point(Point2D);
+  void addPoint(Point2D);
+  void addPoint();
 };
 #endif
